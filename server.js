1477161897bfce0c9ab826c8cc4a42f97cb1694c
ChaitanyaHazarika <chaitanyahazarika9@gmail.com>
2024-08-23 20:31:44 +0530

    first commit after gitignore

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () =>{
+    console.log("Listening on "+ PORT);
+    
+})
